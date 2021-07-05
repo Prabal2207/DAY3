3824039a4438b725fe3469c712988941266ad2d7
Prabal2207 <prabal760@gmail.com>
2021-07-05 07:56:19 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DAY3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //UC1_LengthofLine.Cal_length();
+            //UC2_Checktwolineequal.Checkequal();
+            UC3_checklengthbasedon_endpoints.comparelength();
+
+
+
+
+        }
+    }
+}
